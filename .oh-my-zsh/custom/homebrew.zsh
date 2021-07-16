1c010362be9ed0a76ab722964132f4fb09e5b19b
Bryan Dady <bryan@subsplash.com>
2021-07-15 22:30:24 -0600

    add my zsh startup scripts

--- .oh-my-zsh/custom/homebrew.zsh
+++ .oh-my-zsh/custom/homebrew.zsh
@@ -3,23 +3,23 @@ echo $0
 
 function confirm {
 
-  if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
-  _prompt="$_prompt [y/n] ?"
+    if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
+    _prompt="$_prompt [y/n] ?"
 
   # Loop forever until the user enters a valid response (Y/N or Yes/No).
-  read -rs -k 1 "$_prompt " _response
-  while true; do
-    case "${_response}" in
-    [Yy][Ee][Ss] | [Yy]) # Yes or Y (case-insensitive).
-      return true
-      ;;
-    [Nn][Oo] | [Nn]) # No or N.
-      return false
-      ;;
-    *) # Anything else (including a blank) is invalid.
-      ;;
-    esac
-  done
+    read -rs -k 1 "$_prompt " _response
+    while true; do
+        case "${_response}" in
+            [Yy][Ee][Ss]|[Yy] ) # Yes or Y (case-insensitive).
+                return true
+            ;;
+            [Nn][Oo]|[Nn] )  # No or N.
+                return false
+            ;;
+            * ) # Anything else (including a blank) is invalid.
+            ;;
+        esac
+    done
 }
 
 # PROCEED=$(confirm "Is it time to update Homebrew?")
@@ -33,46 +33,46 @@ BREW_STATUS_FILE="$HOME/.brew_updated"
 # If not found, or older than 3 days, then set BREW_UPDATE == true
 
 #if [ $PROCEED = 1 ]; then
-# Check file exists
-if [ -f $BREW_STATUS_FILE ]; then
-  # get today's date for reference
-  TODAY="$(date -j +%Y%m%d)"
-  #echo "Today is $TODAY"
-  # check file modified date
-  BREWMOD=$(date -j -r $BREW_STATUS_FILE +"%Y%m%d")
-  #echo "Homebrew was last updated on $BREWMOD"
+    # Check file exists
+    if [ -f $BREW_STATUS_FILE ]; then
+        # get today's date for reference
+        TODAY="$(date -j +%Y%m%d)"
+        #echo "Today is $TODAY"
+        # check file modified date
+        BREWMOD=$(date -j -r $BREW_STATUS_FILE +"%Y%m%d")
+        #echo "Homebrew was last updated on $BREWMOD"
 
-  UPDATE_DATE=$(date -j -v+10d -f "%Y%m%d" "$BREWMOD" +"%Y%m%d")
-  #echo "UPDATE_DATE is $UPDATE_DATE"
-  #calculate if $TODAY is at or past n days since $BREWMOD
-  if test $TODAY -lt $UPDATE_DATE; then
-    #echo "BREW_UPDATE=false"
-    BREW_UPDATE=false
-  fi
-#else
-#    echo "File not found!" >&2
-fi
+        UPDATE_DATE=$(date -j -v+10d -f "%Y%m%d" "$BREWMOD" +"%Y%m%d")
+        #echo "UPDATE_DATE is $UPDATE_DATE"
+        #calculate if $TODAY is at or past n days since $BREWMOD
+        if test $TODAY -lt $UPDATE_DATE; then
+            #echo "BREW_UPDATE=false"
+            BREW_UPDATE=false
+        fi
+    #else
+    #    echo "File not found!" >&2
+    fi
 #fi
 
 echo "Homebrew was last updated on $(date -j -f "%Y%m%d" "$BREWMOD" +"%m-%d-%y")"
 #echo "BREW_UPDATE is $BREW_UPDATE"
 
 if [ $BREW_UPDATE = true ]; then
-  echo " ! Updating Homebrew (and casks)"
-  brew update | tee -a $BREW_STATUS_FILE
-  brew upgrade | tee -a $BREW_STATUS_FILE
-  brew cleanup -s | tee -a $BREW_STATUS_FILE
-  brew upgrade --cask | tee -a $BREW_STATUS_FILE # brew cask upgrade
+    echo " ! Updating Homebrew (and casks)"
+    brew update | tee -a $BREW_STATUS_FILE
+    brew upgrade | tee -a $BREW_STATUS_FILE
+    brew cleanup -s | tee -a $BREW_STATUS_FILE
+    brew upgrade --cask | tee -a $BREW_STATUS_FILE # brew cask upgrade
 
-  # now diagnostics
-  #brew doctor
-  #brew cask doctor
+    # now diagnostics
+    #brew doctor
+    #brew cask doctor
 
-  #brew missing
+    #brew missing
 
-  # Create / update the file to declare that HomeBrew was recently updated
-  #echo "touch $BREW_STATUS_FILE"
-  #touch -fm $BREW_STATUS_FILE
+    # Create / update the file to declare that HomeBrew was recently updated
+    #echo "touch $BREW_STATUS_FILE"
+    #touch -fm $BREW_STATUS_FILE
 else
-  echo "Homebrew was already updated recently"
+    echo "Homebrew was already updated recently"
 fi
